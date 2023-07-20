bf91887e9878c01081cca570cb9948476a6e8411
Anna <73963489+Katakiko@users.noreply.github.com>
2023-07-20 09:22:36 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Aufgabe
+Bitte setze eine Anwendung um, die folgende Funktionen beinhaltet.
+
+## Anforderung
+Erstellt werden soll eine Anwendung zur Aufgabenverwaltung (Todo-Liste). Ruft der Nutzer die Anwendung auf, so werden die Aufgaben angezeigt.
+Sind keine Aufgaben vorhanden, wird ein Formular zum Hinzufügen neuer Aufgaben zur Aufgabenliste angezeigt.
+In der gesamten Anwendung hat der Nutzer die Möglichkeit, neue Aufgaben zu erstellen. Eine Aufgabe besteht dabei wenigstens aus folgenden drei Teilen:
+
+- Titel
+- Beschreibung
+- Fälligkeitsdatum
+
+Bei der Eigenschaft 'Titel' handelt es sich um ein Pflichtfeld. Beschreibung und Fälligkeitsdatum sind optional. Bald ablaufende und abgelaufene Aufgaben sollen speziell gekennzeichnet werden.
+Des Weiteren hat der Nutzer die Möglichkeit, Aufgaben zu bearbeiten, abzuschließen oder zu löschen. Es ist ausreichend ein Template für die Struktur zu erstellen. Eine Persistierung der Daten ist nicht notwendig.  Die Anwendung soll intuitiv und nutzerfreundlich sein.
+Nutze moderne technische Ansätze, verwende entsprechende Frameworks oder vorzugsweise eine eigene Gestaltung, um eine übersichtliche und wiederzuerkennende Anwendung zu schaffen. Es gibt für das Erscheinungsbild und die Handhabung keine Vorgaben.
+
+Die Applikation sollte mit den gängigsten Browsern genutzt werden können.
+
+## Nächste Schritte
+
+Wenn du fertig bist, dann lass uns gerne teilhaben.
+Stell uns zudem deinen Quellcode zur Verfügung, so dass einer unserer Entwickler die Anwendung selbst starten kann.
+
+Sobald wir alles gesichtet haben, sprechen wir die weiteren Schritte mit dir ab.
+
+Für Fragen stehen wir dir gern zur Verfügung.
+
+# Für die Entwicklung
+
+## Was benötigst du?
+- Docker
+- ddev
+
+Anleitung: https://ddev.readthedocs.io/en/stable/
+
+## Projektstart
+
+    $ ddev start
+    $ ddev composer install
+    $ ddev import-db --src=db.sql.gz
+
+## Backendnutzer
+    $ Benutzer: admin
+    $ Passwort: Admin1234!
---
+++ config/system/settings.php
@@ -0,0 +1,116 @@
+<?php
+return [
+    'BE' => [
+        'debug' => false,
+        'installToolPassword' => '$argon2i$v=19$m=65536,t=16,p=1$VmxXRU5obkhYblpHNVJJQw$PKugP9jeLX9lN8F52FiRHYYV3SPL2cPvHMpRqk4eHfU',
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'DB' => [
+        'Connections' => [
+            'Default' => [
+                'charset' => 'utf8',
+                'driver' => 'mysqli',
+            ],
+        ],
+    ],
+    'EXTENSIONS' => [
+        'backend' => [
+            'backendFavicon' => '',
+            'backendLogo' => '',
+            'loginBackgroundImage' => '',
+            'loginFootnote' => '',
+            'loginHighlightColor' => '',
+            'loginLogo' => '',
+            'loginLogoAlt' => '',
+        ],
+        'extensionmanager' => [
+            'automaticInstallation' => '1',
+            'offlineMode' => '0',
+        ],
+    ],
+    'FE' => [
+        'cacheHash' => [
+            'enforceValidation' => true,
+        ],
+        'debug' => false,
+        'disableNoCacheParameter' => true,
+        'passwordHashing' => [
+            'className' => 'TYPO3\\CMS\\Core\\Crypto\\PasswordHashing\\Argon2iPasswordHash',
+            'options' => [],
+        ],
+    ],
+    'GFX' => [
+        'processor' => 'GraphicsMagick',
+        'processor_allowTemporaryMasksAsPng' => false,
+        'processor_colorspace' => 'RGB',
+        'processor_effects' => false,
+        'processor_enabled' => true,
+        'processor_path' => '/usr/bin/',
+    ],
+    'LOG' => [
+        'TYPO3' => [
+            'CMS' => [
+                'deprecations' => [
+                    'writerConfiguration' => [
+                        'notice' => [
+                            'TYPO3\CMS\Core\Log\Writer\FileWriter' => [
+                                'disabled' => true,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+    'MAIL' => [
+        'transport' => 'sendmail',
+        'transport_sendmail_command' => '/usr/local/bin/mailhog sendmail test@example.org --smtp-addr 127.0.0.1:1025',
+        'transport_smtp_encrypt' => '',
+        'transport_smtp_password' => '',
+        'transport_smtp_server' => '',
+        'transport_smtp_username' => '',
+    ],
+    'SYS' => [
+        'UTF8filesystem' => true,
+        'caching' => [
+            'cacheConfigurations' => [
+                'hash' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                ],
+                'imagesizes' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'pages' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+                'rootline' => [
+                    'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
+                    'options' => [
+                        'compression' => true,
+                    ],
+                ],
+            ],
+        ],
+        'devIPmask' => '',
+        'displayErrors' => 0,
+        'encryptionKey' => '18418326e97c211d2b918c1a6bc0e95324803e5324c708e8e98154794702db0c8d3b4b5151346e331b0d96959384fddd',
+        'exceptionalErrors' => 4096,
+        'features' => [
+            'security.backend.enforceContentSecurityPolicy' => true,
+            'security.usePasswordPolicyForFrontendUsers' => true,
+        ],
+        'sitename' => 'Todo',
+        'systemMaintainers' => [
+            1,
+        ],
+    ],
+];
