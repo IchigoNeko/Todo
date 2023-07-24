44af0b48c02c9114fbff5656ba0b9c98434f9863
Anna <73963489+Katakiko@users.noreply.github.com>
2023-07-24 14:09:35 +0200

    feat: add edit function, bugfix: validation, checkbox

--- local-packages/todo/Resources/Public/JavaScript/website.js
+++ local-packages/todo/Resources/Public/JavaScript/website.js
@@ -20,7 +20,8 @@
             </div>
             <div class="col-9">
                 <div class="title">${title}</div>
-                <div class="desc"><em>${desc}</em></div>
+                <div id="titleErrorMessage">Dieses Feld ist erforderlich!</div>
+                <div class="desc">${desc}</div>
                 <div class="date">${date}</div>
             </div>
             <div class="col-1 icon-container editButton edit-image-container">
@@ -47,14 +48,77 @@
         });
     }
 
+    const editTitle = (e) => {
+        const editTask = e.target.closest('.task-info');
+        const editTitle = editTask.querySelector('.title');
+        const error = editTask.querySelector('#titleErrorMessage');
+
+        editTitle.setAttribute('contenteditable', 'true');
+        editTitle.addEventListener('keydown', e => {
+            if (e.key === 'Enter') {
+                e.preventDefault();
+                const newTitle = editTitle.innerText.trim();
+                editTitle.innerText = newTitle;
+                if (newTitle !== '') {
+                    editTitle.setAttribute('contenteditable', 'false');
+                    error.style.display = 'none';
+                } else{
+                    error.style.display = 'block';
+                }
+            }
+        })
+    }
+
+    const editDescription = (e) => {
+        const editTask = e.target.closest('.task-info');
+        const editDescription = editTask.querySelector('.desc');
+
+        if (editDescription !== '') {
+            editDescription.setAttribute('contenteditable', 'true');
+            editDescription.addEventListener('keydown', e => {
+                if (e.key === 'Enter') {
+                    editDescription.setAttribute('contenteditable', 'false');
+                }
+            })
+        }
+    }
+
+    const editDate = (e) => {
+        const editTask = e.target.closest('.task-info');
+        const editDateElement = editTask.querySelector('.date');
+        const inputField = document.createElement('input');
+        inputField.type = 'date';
+        inputField.setAttribute('value', editDateElement.innerText);
+        inputField.classList.add('date');
+        editTask.querySelector('.col-9').replaceChild(inputField, editDateElement);
+
+
+       inputField.addEventListener('keydown', e => {
+            if (e.key === 'Enter') {
+                e.preventDefault();
+                const dateField = document.createElement('div');
+                dateField.classList.add('date');
+                dateField.textContent = inputField.value;
+                editTask.querySelector('.col-9').replaceChild(dateField, inputField);
+                validateTime();
+            }
+        })
+    }
+
     submit.addEventListener('click', e => {
         e.preventDefault();
+        const errorMessage = document.getElementById('errorMessage');
         if (title.value !== '') {
             const titleValue = title.value.trim();
             const descValue = description.value.trim();
             const dateValue = date.value;
             generateTaskTemplate(titleValue, descValue, dateValue);
             form.reset();
+        } else {
+            errorMessage.style.display = 'block';
+            title.addEventListener('focus', e =>{
+                errorMessage.style.display = 'none';
+            })
         }
 
         // Check if the task is urgent or too late
@@ -70,10 +134,9 @@
     //Checkbox
     todoList.addEventListener('change', e =>{
         const checkbox = document.querySelectorAll('.form-check-input');
-        console.log(checkbox);
         checkbox.forEach(check =>{
             if (check.checked){
-                check.setAttribute('checked','true');
+                check.setAttribute('checked','checked');
                 const checkedTask = check.closest('.task-info');
                 checkedTask.classList.add('disabled');
                 const editButton = checkedTask.querySelector('.edit');
@@ -82,28 +145,50 @@
                     editButton.remove();
                 }
             }else {
+                check.removeAttribute('checked');
                 const uncheckedTask = check.closest('.task-info');
                 uncheckedTask.classList.remove('disabled');
                 const editButton = uncheckedTask.querySelector('.editButton');
                 if (!editButton) {
-                    const editButton = document.createElement('div');
                     const editButtonContainer = uncheckedTask.querySelector('.edit-image-container');
                     editButtonContainer.classList.add('editButton');
-                    editButton.innerHTML = '<img class="icon edit" src="/_assets/7e5d1bdaae3024dfe65d08456799ed60/Icons/edit.png" alt="edit-Button">';
-                    editButtonContainer.appendChild(editButton);
+                    editButtonContainer.innerHTML = '<img class="icon edit" src="/_assets/7e5d1bdaae3024dfe65d08456799ed60/Icons/edit.png" alt="edit-Button">';
                 }
             }
         })
     });
 
+
     //Delete Task
     todoList.addEventListener('click', e =>{
-        const tasks = document.querySelectorAll('.task-info');
         if(e.target.classList.contains('delete')){
             e.target.parentElement.parentElement.parentElement.remove();
             validateTime();
         }
 
         //editTask
+        const checkbox = e.target.closest('.task-info').querySelector('.form-check-input');
+        if(checkbox.checked){
+            const editTask = e.target.closest('.task-info');
+            const editTitle = editTask.querySelector('.title');
+            const editDescription = editTask.querySelector('.desc');
+            const editDate = editTask.querySelector('.date');
+            console.log(editDate);
+            editTitle.setAttribute('contenteditable', 'false');
+            editDescription.setAttribute('contenteditable', 'false');
+            editDate.setAttribute('disabled', 'true');
+        }else {
+            if (e.target.classList.contains('edit')) {
+                editTitle(e);
+                editDescription(e);
+                editDate(e);
+            }
+            if (e.target.classList.contains('title')) {
+                editTitle(e);
+            }
+            if (e.target.classList.contains('desc')) {
+                editDescription(e);
+            }
+        }
     })
 
